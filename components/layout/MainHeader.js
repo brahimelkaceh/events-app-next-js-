e2553c3ac21611022feab1bda06252651e22c06d
brahimelkaceh <brahimelkaceh@gmail.com>
2023-12-29 15:10:01 +0100

    commit events v1

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -6,7 +6,7 @@ function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href="/">NextEvents</Link>
+        <Link href="/">NextEvents V1</Link>
       </div>
       <nav className={classes.navigation}>
         <ul>
